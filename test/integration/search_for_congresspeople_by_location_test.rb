ca529944c0ff6cdfcef46e2be8f0210be8665ee6
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-08-06 20:50:51 -0600

    Completed first user story

--- test/integration/search_for_congresspeople_by_location_test.rb
+++ test/integration/search_for_congresspeople_by_location_test.rb
@@ -6,6 +6,12 @@ class SearchForCongresspeopleByLocationTest < Capybara::Rails::TestCase
     visit '/'
     assert_equal 200, page.status_code
     assert page.has_content?('Search')
+    fill_in 'Search by Zip', with: '80203'
+    click_button "Search"
+    page.current_path.must_equal search_path
+    assert page.has_content?('Cory Gardner (Republican)')
+    assert page.has_content?('Diana DeGette (Democrat)')
+    assert page.has_content?('Michael Bennet (Democrat)')
   end
 
 end
